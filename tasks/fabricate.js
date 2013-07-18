e004cfa2748476b4f2a32ecd88288ccbb3943a16
Joey Trapp <joey@loadsys.com>
2013-07-18 01:01:45 -0500

    Added modified grunt version with fabricate factored out

---
+++ tasks/fabricate.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var Fabricate = require('fabricate'),
+      desc = 'Uses mincer to build dependencies and apply engines, and then uses uglify to concat and produce source map';
+
+  grunt.registerMultiTask('fabricate', desc, function() {
+    var options  = this.data,
+        done     = this.async();
+
+    new Fabricate(options).build(function(err) {
+      grunt.log.write('Created ' + options.dest.cyan + "\n");
+      done();
+    });
+  });
+};
